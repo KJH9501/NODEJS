554dc83357056cd10ed8e92b11220eacc78a42d4
KJH9501 <skyb_9501@naver.com>
2021-03-15 22:13:00 +0900

    3일차

--- web/main.js
+++ web/main.js
@@ -4,31 +4,6 @@ const express = require('express'); // express 서드파티모듈 생성
 
 const app = express(); //express로 객체생성
 
-users = ['stu1', 'stu2', 'stu3', 'stu4'];
-
-// let server = http.createServer(function (request, response) {
-//   // response.end('<h1>Hello World</h1>');
-//   //console.log(request.url);
-//   if(request.url === '/') {
-//     response.end('<h1>welcome</h1>');
-//     alert('Hi')
-//   }
-//   else if(request.url === '/users') {
-//     response.end('<h1>' + users + '</h2>');
-//   }
-//   else if(request.url.split('/')[1] ==='users') {
-//     const userIdx = request.url.split('/')[2];
-//     const username = users[userIdx -1];
-//     response.end('<h1>'+username+'</h1>');
-//   }
-//   //split메소드사용
-//   else{
-//     response.end('<h1>no page</h1>');
-//   }
-// }); //http객체에 createServer메소드 호출.
-
-//server.listen(3000); // listen - 클라이언트 요청 대기 / 포트 3000
-
 app.get('/', (request, response) => {
   response.end('<h1>Welcome!</h1>');
 });
@@ -48,4 +23,3 @@ app.get('*', (request, response) => {
 });
 
 app.listen(3000);
-
